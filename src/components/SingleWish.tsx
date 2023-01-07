5ef92c280bd055a8fb0698888d28286997f26996
Chananabanana <hwane94@gmail.com>
2023-01-08 00:18:13 +0900

    Update SingleWish.tsx

--- src/components/SingleWish.tsx
+++ src/components/SingleWish.tsx
@@ -10,15 +10,35 @@ interface Props {
 
 
 const SingleWish = ({ wish, wishes, setWishes }: Props) => {
+
+  const handleDone = (id: string) => {
+    setWishes(wishes.map((wish) =>
+      wish.id === id ? { ...wish, isDone: !wish.isDone } : wish)
+    )
+  }
+
+  const handleEdit = (id: string) => {
+
+  }
+
+  const handleDelete = (id: string) => {
+    setWishes(wishes.filter(wish => wish.id !== id))
+  }
+
+
   return (
     <form className='single-wish'>
-      <span className='single-wish-text'>
-        {wish.wish}
-      </span>
+      {
+        wish.isDone ? (
+          <span className='single-wish-text'><del>{wish.wish}</del></span>
+        ) : (
+          <span className='single-wish-text'>{wish.wish}</span>
+        )
+      }
       <div className='icons'>
-        <span className='icon'><i className="fa-solid fa-check"></i></span>
-        <span className='icon'><i className="fa-solid fa-pen-to-square"></i></span>
-        <span className='icon'><i className="fa-solid fa-trash"></i></span>
+        <span className='icon' onClick={() => handleDone(wish.id)}><i className="fa-solid fa-check"></i></span>
+        <span className='icon' onClick={() => handleEdit(wish.id)}><i className="fa-solid fa-pen-to-square"></i></span>
+        <span className='icon' onClick={() => handleDelete(wish.id)}><i className="fa-solid fa-trash"></i></span>
       </div>
     </form>
   )
