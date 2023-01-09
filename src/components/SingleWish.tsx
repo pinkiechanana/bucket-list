6147ddc4dd5f47baf78f907defc27d0509a93bc7
Chananabanana <hwane94@gmail.com>
2023-01-09 15:14:02 +0900

    final

--- src/components/SingleWish.tsx
+++ src/components/SingleWish.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Wish } from '../model'
 import './styles.css'
 
@@ -20,11 +20,16 @@ const SingleWish = ({ wish, wishes, setWishes }: Props) => {
     )
   }
 
-  const handleEdit = (id: string) => {
-    if (!edit && !wish.isDone) {
-      setEdit(!edit);
-    }
-  }
+  const handleEdit = (e: React.FormEvent, id: string) => {
+    e.preventDefault();
+
+    setWishes(wishes.map((wish) => (
+      wish.id === id ? { ...wish, wish: editWish } : wish ))
+      );
+      setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleDelete = (id: string) => {
     setWishes(wishes.filter(wish => wish.id !== id))
@@ -32,17 +37,23 @@ const SingleWish = ({ wish, wishes, setWishes }: Props) => {
 
 
   return (
-    <form className='single-wish'>
-      {
-        wish.isDone ? (
-          <span className='single-wish-text'><del>{wish.wish}</del></span>
-        ) : (
-          <span className='single-wish-text'>{wish.wish}</span>
-        )
-      }
+    <form className='single-wish' onSubmit={(e) => handleEdit(e, wish.id)}>
+      {edit ? (
+        <input value={editWish} onChange={(e) => setEditWish(e.target.value)} className="single-wish-text"></input>
+      ) : wish.isDone ? (
+        <span className='single-wish-text'><del>{wish.wish}</del></span>
+      ) : (
+        <span className='single-wish-text'>{wish.wish}</span>
+      )}
+
       <div className='icons'>
         <span className='icon' onClick={() => handleDone(wish.id)}><i className="fa-solid fa-check"></i></span>
-        <span className='icon' onClick={() => handleEdit(wish.id)}><i className="fa-solid fa-pen-to-square"></i></span>
+        <span className='icon' onClick={() => {
+          if (!edit && !wish.isDone) {
+            setEdit(!edit)
+          }
+        }}>
+          <i className="fa-solid fa-pen-to-square"></i></span>
         <span className='icon' onClick={() => handleDelete(wish.id)}><i className="fa-solid fa-trash"></i></span>
       </div>
     </form>
